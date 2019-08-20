944c15e8e838f9c02d679d029ec73444dc7acd6a
vineshghela <vineshghela@gmail.com>
2019-08-20 13:07:04 +0100

    initial commit

---
+++ src/main/java/com/QA/App.java
@@ -0,0 +1,11 @@
+package com.QA;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println("Hello World!");
+    }
+}
